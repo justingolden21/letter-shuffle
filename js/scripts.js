a87ee5777619ab496eb88b10c8c48fbe1fb19ddd
Justin Golden <justingolden21@gmail.com>
2021-01-28 20:46:38 -0800

    css

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ const shuffle = arr => arr.sort((a,b)=>0.5-Math.random());
 
 window.onload = ()=> {
 	document.getElementById('submit').onclick = ()=> {
-		const chars = document.getElementById('input').value.split('');
+		const chars = document.getElementById('input').value.split('').filter(s=>s!=' ');
 		let combos = [];
 		for(let i=0; i<10; i++) {
 			combos.push(shuffle([...chars]));
